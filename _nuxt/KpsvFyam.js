06c7f528a839b4a1261dbf23a69351a942c4e53a
empirecitynyc <clement@empirecitynyc.com>
2025-11-07 20:50:36 +0000

    Deploying to gh-pages from @ empirecitynyc/empire@60582a254713972cbf2f00ecec9fdbeb7e46b0a8 🚀

---
+++ _nuxt/KpsvFyam.js
@@ -0,0 +1,1 @@
+import{r as t}from"./DwZAkNkx.js";import a from"./D2tH8z8c.js";import{z as e,H as n,i as s,w as i,W as c}from"./DTuZjYmI.js";import"./BN_7HF1G.js";const d={__name:"ProseCaution",setup(p){const o=e();return(r,f)=>(s(),n(a,{color:"error",icon:c(o).ui.icons.caution},{default:i(()=>[t(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
